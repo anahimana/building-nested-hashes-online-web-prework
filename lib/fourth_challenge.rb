fa6ce085f17381a416cea23f3569d3a5da197628
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 16:00:01 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,7 +11,7 @@ def fourth_challenge
           :attitude => "worried",
         },
         {
-          :name => "value",
+          :name => "Mercutio",
           :age => "value",
           :attitude => "value",
         }
