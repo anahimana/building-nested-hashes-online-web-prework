fef9b8420e90f8d61a1b91f3ac70ca2288d07161
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 15:46:18 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -3,6 +3,9 @@ def first_challenge
    epic_tragedy = {
      :montague => {
        :patriarch => {},
+       :patriarch => {},
+       :patriarch => {},
+       :patriarch => {},
      },
      :capulets => {}
    }
