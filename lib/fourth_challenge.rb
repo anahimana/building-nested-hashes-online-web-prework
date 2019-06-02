7e85f3c9b56a4e39208633878ce563dcdec9813f
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 15:59:53 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,7 +7,7 @@ def fourth_challenge
       :hero_friends => [
         {
           :name => "Benvolio",
-          :age => "value",
+          :age => "17",
           :attitude => "value",
         },
         {
