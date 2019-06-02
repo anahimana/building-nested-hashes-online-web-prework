965c6a5308022be82c395ce99365c142a4f8e5af
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 15:55:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,8 +18,8 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lady Montague",
-        :age => "54"
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
